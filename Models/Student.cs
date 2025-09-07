ff986481690621824a9fbbd88f55a02ebea13dbb
G Arunkumar <112071741+GArunkumarEnagaluru@users.noreply.github.com>
2025-09-07 10:36:11 +0530

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,4 +7,21 @@ namespace StudentAPI.Models
         public string? SchoolName { get; set; }
         public ExamResult? ExamResult { get; set; }
     }
+
+    public class StudentPostModel
+    {
+        public string? Name { get; set; }
+        public string? SchoolName { get; set; }
+        public ExamResultPostModel? ExamResult { get; set; }
+    }
+
+    public class ExamResultPostModel
+    {
+        public int English { get; set; }
+        public int Telugu { get; set; }
+        public int Hindi { get; set; }
+        public int Mathematics { get; set; }
+        public int Physics { get; set; }
+        public int Social { get; set; }
+    }
 }
