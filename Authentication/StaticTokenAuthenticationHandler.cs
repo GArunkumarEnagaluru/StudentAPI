e065cbbc4c20852766580abb88f0e4c880ccb28e
G Arunkumar <112071741+GArunkumarEnagaluru@users.noreply.github.com>
2025-09-08 21:29:55 +0530

    Create StaticTokenAuthenticationHandler.cs

---
+++ Authentication/StaticTokenAuthenticationHandler.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace StudentAPI.Authentication
+{
+    public class StaticTokenAuthenticationHandler : AuthenticationHandler<StaticTokenAuthenticationSchemeOptions>
+    {
+        public StaticTokenAuthenticationHandler(
+            IOptionsMonitor<StaticTokenAuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.TryGetValue("Authorization", out var authorizationHeader))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Authorization header not found."));
+            }
+
+            var token = authorizationHeader.ToString();
+            if (token != StaticTokenAuthenticationSchemeOptions.StaticToken)
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Invalid token."));
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "StaticUser")
+            };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+    }
+}
